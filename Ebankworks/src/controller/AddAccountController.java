9ab2681c0ebc8c8c45972b8951cce61ff332cfb2
saurav012 <53165012+saurav012@users.noreply.github.com>
2019-11-30 13:45:20 +0530

    2nd commit for the github repositery

--- Ebankworks/src/controller/AddAccountController.java
+++ Ebankworks/src/controller/AddAccountController.java
@@ -1,5 +1,11 @@
 package controller;
 
 public class AddAccountController {
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hi i m  new to github for now learing how to use github");
+	}
 
 }
